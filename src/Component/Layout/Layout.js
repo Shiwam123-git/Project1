fd49f79f68827f51d84d151a68749be3d7b843d4
Kumar Shiwam <kumar.shiwam@ad.infosys.com>
2025-02-08 16:41:29 +0530

    initial commit

---
+++ src/Component/Layout/Layout.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
+
+function Layout({Children}) {
+  return (
+    <>
+    
+    
+    
+    
+    <Header />
+    <div>{Children}</div>
+    <Footer />
+    </>
+   
+  )
+}
+
+export default Layout
